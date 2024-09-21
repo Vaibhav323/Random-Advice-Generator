ca9b4709f42c77cbb14b5e719fc0b25a52c96bb2
Vaibhav Londhe <vmlondhe25@gmail.com>
2024-09-21 23:23:09 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(() => getAdvice(), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="advice-container">
+        <h1 className="advice-text">{advice}</h1>
+        <button onClick={getAdvice} className="advice-button">
+          Get Advice
+        </button>
+        <Message count={count} />
+      </div>
+      <DarkModeToggle />
+    </>
   );
 }
 
-export default App;
+function Message({ count }) {
+  return (
+    <p className="advice-info">
+      You have read <strong className="advice-count">{count}</strong>{" "}
+      {count === 1 ? "piece" : "pieces"} of advice.
+    </p>
+  );
+}
+function DarkModeToggle() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  function toggleDarkMode() {
+    setIsDarkMode((prevMode) => !prevMode);
+  }
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+  return (
+    <div className="dark-mode-toggle">
+      <input
+        type="checkbox"
+        id="toggle-dark-mode"
+        className="toggle-checkbox"
+        checked={isDarkMode}
+        onChange={toggleDarkMode}
+      />
+      <label for="toggle-dark-mode" className="toggle-label">
+        <span className="toggle-inner"></span>
+        <span className="toggle-switch"></span>
+      </label>
+    </div>
+  );
+}
